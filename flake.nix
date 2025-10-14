1ee86520c5ce00f5581dc49d2e69f827fd41f4a4
Wolf Mermelstein <wolf@404wolf.com>
2025-10-14 11:52:36 -0400

    Set up project repo

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "CSDS340 Case Study 1";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python3.withPackages (
+          ps: with ps; [
+            black
+            scikit-learn
+            matplotlib
+            numpy
+            pandas
+          ]
+        );
+
+        treefmtconfig = inputs.treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            black.enable = true;
+            prettier.enable = true;
+          };
+        };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.typst
+              pkgs.pyright
+              pkgs.nil
+              pkgs.nixd
+              python
+            ];
+          };
+        };
+        formatter = treefmtconfig.config.build.wrapper;
+        checks = {
+          formatting = treefmtconfig.build.check self;
+        };
+      }
+    );
+}
